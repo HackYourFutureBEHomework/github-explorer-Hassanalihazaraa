66b1bfb743fa59b54085ecddb2c32156bc3eb2b6
Hassan <39343120+Hassanalihazaraa@users.noreply.github.com>
2018-08-03 19:54:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.responseType = 'json';
-        xhr.onload = () => {
-            if (xhr.status < 400) {
-                cb(null, xhr.response);
-            } else {
-                cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
-            }
-        };
+         xhr.onload = () => {
+        if (xhr.status < 400) {
+          resolve(xhr.response);
+        } else {
+          reject(xhr.response);
+        }
+      };
     xhr.onerror = () => cb(new Error('Network request failed'));
     xhr.send();
   }
