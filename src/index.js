3fb0a5e1eeb08fc71f2f565b49ff5e8aceb4b000
Hassan <39343120+Hassanalihazaraa@users.noreply.github.com>
2018-08-03 19:45:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -31,16 +31,65 @@
   }
 
   function main(url) {
-    fetchJSON(url, (err, data) => {
-      const root = document.getElementById('root');
-      if (err) {
-        createAndAppend('div', root, { html: err.message, class: 'alert-error' });
-      } else {
-        createAndAppend('pre', root, { html: JSON.stringify(data, null, 2) });
-      }
-    });
+     const root = document.getElementById("root");
+    fetchJSON(url)
+      .then(data => {
+        const header = createAndAppend("header", root, {class: "header"});
+        const headerName = createAndAppend("div", header, {class: "headerName", html: "HYF Repositories"});
+        
+        const select = createAndAppend("select", headerName, {class: "select", html: "Select the Repositories"});
+        data.forEach(repo => {
+          createAndAppend("option", select, {class: "option", html: repo.name });
+          });
+        const mainContainer = createAndAppend("div", root, {class: "mainContainer" });
+        const left = createAndAppend("div", mainContainer, {class: "contributorsDetails" });
+        const right = createAndAppend("div", mainContainer, {class: "contributors" });
+
+        selectButton.addEventListener("change", () => {
+          const repo = data.find(r => r.name === select.value);
+          left.innerHTML = "";
+          right.innerHTML = "";
+          renderRepo(left, repo);
+          renderContributors(right, repo); });
+      })
+      .catch(err => {
+        const root = document.getElementById("root");
+
+        createAndAppend("div", root, {html: err.message, class: "errorAlert" });
+      });
   }
 
+  function renderRepo(parent, repo) {
+    const details = createAndAppend("div", parent, {class: "details"});
+    const name = createAndAppend("h4", details, {html: "Repository: "});
+    createAndAppend("a", name, {class: "name-url", html: `${repo.name}`, href: repo.html_url});
+    if(repo.description !== null){
+        const description  = createAndAppend("h4", details, {html: "Description: "});
+        createAndAppend("p", description, {html: `${repo.description }`});
+    }
+    const forks = createAndAppend("h4", details, {html: "Forks: "});
+    createAndAppend("p", forks, {html: ` ${repo.forks_count}`});
+    const updated = createAndAppend("h4", details, {html: "Updated: "});
+    createAndAppend("p", updated, {html: `${repo.updated_on}`});
+
+
+  function renderContributors(parent, repo) {
+    const url = repo.contributors_url;
+    fetchJSON(url)
+      .then(contributors => {
+        const $contributorsList = createAndAppend("ul", parent, {class: "contributorlist", html: "Contributions" });
+        contributors.forEach(contributor => {
+          const $contributorData = createAndAppend("li", $contributorsList, {class: "contributorData" });
+          createAndAppend("img", $contributorData, {class: "contributorImage", src: contributor.avatar_url });
+          createAndAppend("a", $contributorData, {html: `${contributor.login}`, class: "contributorsLink", href: contributor.html_url, target: "_blank" });
+          createAndAppend("h4", $contributorData, {class: "contribution", html: contributor.contributions });
+        });
+      })
+      .catch(err => {
+        createAndAppend("div", parent, {html: err.message, class: "errorAlert" });
+      });
+  }
+  
   const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
 
   window.onload = () => main(HYF_REPOS_URL);
